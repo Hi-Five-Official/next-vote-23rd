8512b666eb1c5ebf28707c02bf5bb33fb08dd33e
YuminPark <p353700@gmail.com>
2026-05-11 22:41:34 +0900

    [SETTING] Next.js 프로젝트 초기세팅

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "next-vote-23rd",
+  description: "next-vote-23rd",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko" className="h-full antialiased">
+      <body className="flex min-h-full flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div className="text-purple-600">page</div>;
+};
+
+export default page;
