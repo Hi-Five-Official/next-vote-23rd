83c272a17f986db6a947283cf4f760f56d7bf96b
KwonOjin <ojin630@naver.com>
2026-05-13 03:24:15 +0900

    #6 [MOD] 공통 버튼 컴포넌트 cn 반영, rounded 수정 및 화살표 함수 활용

--- app/components/common/Button.tsx
+++ app/components/common/Button.tsx
@@ -1,19 +1,26 @@
+import { cn } from "@/app/lib/utils/cn";
+
 interface ButtonProps {
   children: React.ReactNode;
   isSelected?: boolean;
   onClick?: () => void;
 }
 
-function Button({ children, isSelected = false, onClick }: ButtonProps) {
+const Button = ({ children, isSelected = false, onClick }: ButtonProps) => {
   return (
     <button
       type="button"
       onClick={onClick}
-      className={`text-body2-m md:text-heading2-m border-gray-60 inline-flex cursor-pointer items-center justify-center rounded-[var(--radius-50)] border px-8 py-3 text-purple-50 md:px-14 md:py-3 ${isSelected ? "text-body2-sb md:text-heading2-sb border-purple-50 bg-purple-50 text-white" : "hover:bg-purple-40 border-purple-50 bg-white text-purple-50 hover:text-white"}`}
+      className={cn(
+        "text-body2-m md:text-heading2-m rounded-50 inline-flex cursor-pointer items-center justify-center border border-purple-50 px-8 py-3 text-purple-50 transition-colors md:px-14 md:py-3",
+        isSelected
+          ? "text-body2-sb md:text-heading2-sb bg-purple-50 text-white"
+          : "hover:bg-purple-40 bg-white hover:text-white",
+      )}
     >
       {children}
     </button>
   );
-}
+};
 
 export default Button;
