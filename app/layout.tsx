137d412dd8c81103840d9229b2666dbd88cdbc14
YuminPark <p353700@gmail.com>
2026-05-13 03:56:28 +0900

    #5 [REFACTOR] 그라데이션 제거 및 칩 컴포넌트 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,7 @@ export default function RootLayout({
     <html lang="ko" className="h-full overflow-hidden antialiased">
       <body className="flex h-full flex-col overflow-hidden bg-white">
         <Header />
-        <main className="relative flex flex-1 items-center justify-center">
-          <div className="bg-purple-fade pointer-events-none absolute top-0 right-0 left-0 h-22.5" />
-          {children}
-        </main>
-        <div className="bg-purple-fade z-layout fixed right-0 bottom-0 left-0 h-22.5 rotate-180" />
+        <main className="relative flex flex-1 items-center justify-center">{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+import Chip from "@/app/components/common/Chip";
+import CTA from "@/app/components/common/CTA";
+
 const page = () => {
   return (
-    <div className="flex flex-col items-center gap-6">
-      <h1 className="text-heading1-sb text-black md:text-[40px] md:leading-[135%] md:font-semibold md:tracking-[-0.8px]">
-        🏆<span className="text-purple-60">2026</span> CEOS
-        <br />
-        23RD AWARDS
-      </h1>
+    <div>
+      <div className="flex flex-col items-center gap-6">
+        <h1 className="text-heading1-sb text-black md:text-[40px] md:leading-[135%] md:font-semibold md:tracking-[-0.8px]">
+          🏆<span className="text-purple-60">2026</span> CEOS
+          <br />
+          23RD AWARDS
+        </h1>
+        <Chip label="투표 분야" />
+        <div className="text-body2-sm md:text-heading2-m flex flex-col gap-3 pb-30 text-left">
+          <p># 프론트엔드 파트장 투표</p>
+          <p># 백엔드 파트장 투표</p>
+          <p># 데모데이 파트장 투표</p>
+        </div>
+        <CTA label="투표하러 가기" />
+      </div>
+      <p className="text-gray-80 text-body2-m md:text-heading2-m pt-3 text-center">
+        현재 총 20건의 투표가 진행되었어요!
+      </p>
     </div>
   );
 };
