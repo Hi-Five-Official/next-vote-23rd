d7455b4c71a04449990bd2db4df9d25474f82fa8
KwonOjin <ojin630@naver.com>
2026-05-13 18:01:26 +0900

    #8 [FEAT] 투표 페이지 UI 구현 및 Mock data 생성

---
+++ src/data/members.ts
@@ -0,0 +1,25 @@
+export const frontendMembers = [
+  { name: "구민교", university: "Ewha. W. Univ" },
+  { name: "권오진", university: "Hongik Univ." },
+  { name: "김민서", university: "Ewha. W. Univ" },
+  { name: "김홍엽", university: "Hongik Univ." },
+  { name: "남기림", university: "Ewha. W. Univ" },
+  { name: "박유민", university: "Hongik Univ." },
+  { name: "오유진", university: "Yonsei Univ." },
+  { name: "이승연", university: "Hongik Univ." },
+  { name: "이윤서", university: "Hongik Univ." },
+  { name: "황영준", university: "Hongik Univ." },
+];
+
+export const backendMembers = [
+  { name: "김도현", university: "Hongik Univ." },
+  { name: "김동욱", university: "Hongik Univ." },
+  { name: "김태익", university: "Hongik Univ." },
+  { name: "김태희", university: "Hongik Univ." },
+  { name: "안준석", university: "Hongik Univ." },
+  { name: "임종훈", university: "Hongik Univ." },
+  { name: "오지송", university: "Ewha. W. Univ." },
+  { name: "최승원", university: "Hongik Univ." },
+  { name: "최우혁", university: "Hongik Univ." },
+  { name: "황신애", university: "Hongik Univ." },
+];
